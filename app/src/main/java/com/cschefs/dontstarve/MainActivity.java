71bfaaa07d44db69d900b1612f704e8d570f522c
Sebastian Dang <s1dang@ucsd.edu>
2015-11-11 18:56:05 -0800

    Function
    Cleaned up code, added function to the search page and added context
    menu for delete.

--- app/src/main/java/com/cschefs/dontstarve/MainActivity.java
+++ app/src/main/java/com/cschefs/dontstarve/MainActivity.java
@@ -1,73 +1,109 @@
 package com.cschefs.dontstarve;
 
-import android.app.SearchManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.SearchView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    /** ListView listIngredients links to list_ingredients. Displays list of ingredients chosen */
+    private ListView listIngredients;
+    /** ArrayList arrayIngredients allows for storage of ingredients chosen */
+    ArrayList<String> arrayIngredients;
+    /** Other variables needed */
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Automatically call to set up the activity page.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Set up the UI
+        listIngredients = (ListView) findViewById(R.id.list_ingredients);
+        Button searchBtn = (Button) findViewById(R.id.search_button);
+        Button findBtn = (Button) findViewById(R.id.find_button);
+        TextView emptyText = (TextView)findViewById(R.id.empty_list);
+        //Set up the ArrayList
+        if (savedInstanceState == null) { arrayIngredients = new ArrayList<String>();}
+        else { arrayIngredients = savedInstanceState.getStringArrayList("savedList");}
+        //Add items and adapter to ListView
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.ingredient_name, arrayIngredients);
+        listIngredients.setAdapter(adapter);
+        //Register the listView for context menu functionality.
+        registerForContextMenu(listIngredients);
+        //Set the textView to display when ListView is empty.
+        listIngredients.setEmptyView(emptyText);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        // Associate searchable configuration with the SearchView
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
-
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
     }
-
-    public void selectItem(View view){
-        boolean checked = ((CheckBox) view).isChecked();
-
-
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
-
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the user's current state
+        outState.putStringArrayList("savedList", arrayIngredients);
+    }
     /** Function is called when the user clicks the Search Ingredients Button
-     *  Opens Search Activity page with search functionality embedded */
+     *  Opens Search Activity with search functionality embedded */
     public void searchPage(View view) {
         // Do something in response to button
-        Intent intent = new Intent(this, SearchableActivity.class);
-        startActivity(intent);
-        setContentView(R.layout.activity_searchable);
+        Intent searchForIngredient = new Intent(this, SearchableActivity.class);
+        startActivityForResult(searchForIngredient, 100);
     }
+    /** Function to read the result from newly created activities */
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //If the code is from "Search Ingredients" page
+        if(resultCode == 100){
+            String ingredient_to_add = data.getStringExtra("received_ingredient");
+            if (ingredient_to_add != null) {
+                arrayIngredients.add(ingredient_to_add);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
+    /** Function to open a context menu*/
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_context, menu);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int position = info.position;
+        switch (item.getItemId()) {
+            case R.id.context_delete: {
+                arrayIngredients.remove(position);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
 }
--- app/src/main/java/com/cschefs/dontstarve/SearchableActivity.java
+++ app/src/main/java/com/cschefs/dontstarve/SearchableActivity.java
@@ -1,79 +1,84 @@
 package com.cschefs.dontstarve;
 
-import android.app.SearchManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SearchView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class SearchableActivity extends AppCompatActivity {
-
-    /** Listview listSearch links to list_ingredients_search
-     *  EditText inputSearch links to input_search  */
+    /** ListView listSearch links to list_ingredients_search. Displays list of ingredients */
     private ListView listSearch;
-    EditText inputSearch;
-
-    ArrayAdapter<String> adapter;
-    ArrayList<HashMap<String, String>> ingredientList;
-
+    /** EditText inputSearch links to input_search. Displays search bar */
+    private EditText inputSearch;
+    /** String ingredient_name parses the selected ingredients name */
+    private String ingredient_name;
+    /** Other variables needed */
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Automatically call to set up the activity page.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
-
+        //Set up the list of ingredients
         String ingredients[] = getResources().getStringArray(R.array.string_ingredients);
-
-        //Listview and EditText set up
+        //Set up the UI
         listSearch = (ListView) findViewById(R.id.list_ingredients_search);
         inputSearch = (EditText) findViewById(R.id.input_search);
-
-        // Adding items to ListView
+        Button backBtn = (Button) findViewById(R.id.back_button_search);
+        Button addBtn = (Button) findViewById(R.id.add_button_search);
+        //Add items and adapter to ListView
         adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.ingredient_name, ingredients);
         listSearch.setAdapter(adapter);
-
+        //Text watcher is used to filter the search and display as you type.
         inputSearch.addTextChangedListener(new TextWatcher() {
-
             @Override
             public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
-                // When user changed the Text
+                // When user changes text in inputSearch
                 SearchableActivity.this.adapter.getFilter().filter(cs);
             }
-
             @Override
             public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
                                           int arg3) {
                 // TODO Auto-generated method stub
-
             }
-
             @Override
             public void afterTextChanged(Editable arg0) {
                 // TODO Auto-generated method stub
             }
         });
-
-        // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            if (doMySearch(query)){
+        //Set the selected Ingredient to the search bar.
+        listSearch.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3)
+            {
+                Object ingredient_obj = arg0.getItemAtPosition(arg2);
+                ingredient_name = ingredient_obj.toString();
+                inputSearch.setText(ingredient_name);
+            }});
+        //End the activity and go back.
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                finish();
             }
-        }
+        });
+        //Add the selected item.
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent search_done = getIntent();
+                search_done.putExtra("received_ingredient", ingredient_name);
+                setResult(100, search_done);
+                finish();
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -81,26 +86,5 @@ public class SearchableActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
         return true;
-    }
-    protected boolean doMySearch(String query) {
-        boolean found = false;
-        ArrayList al = new ArrayList();
-        al.add("potato");
-        al.add("onion");
-        al.add("eggs");
-        al.add("milk");
-
-        for(int i = 0; i < al.size(); i++) {
-            if (al.get(i).equals(query)) {
-                found = true;
-            }
-        }
-        return found;
-    }
-    public void mainPage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        setContentView(R.layout.activity_main);
     }
 }
